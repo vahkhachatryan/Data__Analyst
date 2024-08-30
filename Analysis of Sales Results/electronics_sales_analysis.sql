3be11c8a445505a477aa7e2ef01815b62721d137
Vahe <124351893+vahkhachatryan@users.noreply.github.com>
2024-08-30 12:31:52 +0400

    Delete Analysis of Sales Results directory

--- Analysis of Sales Results/electronics_sales_analysis.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-	quantity,
-	product_category,
-    SUM (total_amount) AS sum_total_amount,
-	AVG (price_per_unit) AS avg_price_per_unit
-FROM project_1
-	WHERE product_category = 'Electronics'
-GROUP BY quantity, product_category
-ORDER BY quantity
